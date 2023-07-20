c79559b7d3a1ed4b276eb18b388b8848a1ef51c4
Brendan Burke <101018068+brendanfburke@users.noreply.github.com>
2023-07-20 19:29:20 -0400

    created number randomizer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { supabase } from "../src/lib/api";
 import { useEffect, useState } from 'react';
+import { random } from 'lodash';
 
 
 function App() {
 
   const [movies, setMovies] = useState(null)
+  const [movieSelection, setMovieSelection] = useState("")
 
   const fetchNumbers = async () => {
     let { data: dvds, error } = await supabase
@@ -20,21 +22,33 @@ function App() {
     fetchNumbers()
   }, [])
 
+  function randomMovie(num) {
+    let choice = Math.floor(Math.random() * (num + 1))
+    return choice
+  }
 
+  function getNewMovie() {
+    let number = randomMovie(movies.length-1)
+    setMovieSelection(movies[number].Title)
+    console.log(number, movieSelection)
+
+
+  }
+
+  
   const loaded = () => {
-
-    console.log(movies)
+    
+    let ourMovie = randomMovie(movies.length)
+    
+    // console.log(movies)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div>
-            {movies.map((dvd, key) => {
-              return (
-                <div key={key}>{dvd.numbers}</div>
-              )
-            })}
+          <div> 
+            {movieSelection}
           </div>
+          <button onClick={getNewMovie}>Get a Movie</button>
         </header>
       </div>
     );
